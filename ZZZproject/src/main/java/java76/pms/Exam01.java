0c6d5b0625ce2b4e7896f179f5b3b1ad83577a76
SR <zxqw1541@naver.com>
2015-12-28 10:19:30 +0900

    오호라

--- ZZZproject/src/main/java/java76/pms/Exam01.java
+++ ZZZproject/src/main/java/java76/pms/Exam01.java
@@ -3,6 +3,15 @@ package java76.pms;
 public class Exam01 {
   public void test() {
     System.out.println("d 오호라~");
+    System.out.println("d 오호라~");
+    System.out.println("d 오호라~");
+    System.out.println("d 오호라~");
+    System.out.println("d 오호라~");
+    System.out.println("d 오호라~");
+    System.out.println("d 오호라~");
+    System.out.println("d 오호라~");
+    System.out.println("d 오호라~");
+    
   }
 
 }
